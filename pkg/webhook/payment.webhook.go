74f687489b26dbc45a0b8dc2e9cf73bed7a2a95a
harley winston <harleywinston1993@proton.me>
2023-05-19 00:11:19 +0330

    [ FIX ] parse invoice

--- pkg/webhook/payment.webhook.go
+++ pkg/webhook/payment.webhook.go
@@ -21,6 +21,7 @@ type PaymentWebhooks struct {
 }
 
 func (wh *PaymentWebhooks) CryptoBotWebhook(ctx *gin.Context) {
+	var messages []tgbotapi.MessageConfig
 	body, err := io.ReadAll(ctx.Request.Body)
 	if err != nil {
 		log.Println(err.Error())
@@ -40,62 +41,68 @@ func (wh *PaymentWebhooks) CryptoBotWebhook(ctx *gin.Context) {
 		return
 	}
 
-	payload, ok := update.Payload.(string)
-	if !ok {
-		log.Println("Type assertion error!")
-		ctx.JSON(consts.BIND_JSON_ERROR.Code, gin.H{
-			"message": consts.BIND_JSON_ERROR.Message,
-			"detail":  "Type assertion error!",
-		})
-		return
-	}
-	payloadData := strings.Split(strings.ReplaceAll(payload, " ", ""), "|")
-	if len(payloadData) < 4 {
-		log.Println("payload len")
-		ctx.JSON(consts.CRYPTO_BOT_PAYLOAD_ERROR.Code, gin.H{
-			"message": consts.CRYPTO_BOT_PAYLOAD_ERROR.Message,
-			"detail":  "payload data len is less than 4!",
-		})
-	}
-	chatID, err := strconv.ParseInt(payloadData[0], 10, 64)
-	if err != nil {
-		log.Println("invalid chat id")
-		ctx.JSON(consts.PARSE_STRING_ERROR.Code, gin.H{
-			"message": consts.PARSE_STRING_ERROR.Message,
-			"detail":  err.Error(),
-		})
-		return
-	}
-	email := payloadData[1]
-	username := payloadData[2]
-	fuckedUser, err := strconv.ParseBool(payloadData[3])
-	if err != nil {
-		log.Println(err.Error())
-		ctx.JSON(consts.PARSE_STRING_ERROR.Code, gin.H{
-			"message": consts.PARSE_STRING_ERROR.Message,
-			"detail":  err.Error(),
-		})
-		return
-	}
-	user := models.UserModel{
-		ChatID:     chatID,
-		Email:      email,
-		Username:   username,
-		FuckedUser: fuckedUser,
-	}
+	switch update.UpdateType {
+	case cryptobot.InvoicePaidWebhookUpdateType:
+		invoice, err := cryptobot.ParseInvoice(body)
+		if err != nil {
+			log.Println(err.Error())
+			ctx.JSON(consts.BIND_JSON_ERROR.Code, gin.H{
+				"message": consts.BIND_JSON_ERROR.Message,
+				"detail":  err.Error(),
+			})
+			return
+		}
 
-	messages, err := wh.buyService.ProceedAfterPayment(user)
-	if err != nil {
-		log.Println(err.Error())
-		msg1 := tgbotapi.NewMessage(user.ChatID, err.Error())
-		msg2 := tgbotapi.NewMessage(user.ChatID, consts.INTERNAL_ERROR_CONTACT_SUPPORT_MESSGE)
-		if _, err := wh.Bot.Send(msg1); err != nil {
-			log.Println(err.Error())
+		payloadData := strings.Split(strings.ReplaceAll(invoice.Payload, " ", ""), "|")
+		if len(payloadData) < 4 {
+			log.Println("payload len")
+			ctx.JSON(consts.CRYPTO_BOT_PAYLOAD_ERROR.Code, gin.H{
+				"message": consts.CRYPTO_BOT_PAYLOAD_ERROR.Message,
+				"detail":  "payload data len is less than 4!",
+			})
 		}
-		if _, err := wh.Bot.Send(msg2); err != nil {
-			log.Println(err.Error())
+		chatID, err := strconv.ParseInt(payloadData[0], 10, 64)
+		if err != nil {
+			log.Println("invalid chat id")
+			ctx.JSON(consts.PARSE_STRING_ERROR.Code, gin.H{
+				"message": consts.PARSE_STRING_ERROR.Message,
+				"detail":  err.Error(),
+			})
+			return
 		}
-		return
+		email := payloadData[1]
+		username := payloadData[2]
+		fuckedUser, err := strconv.ParseBool(payloadData[3])
+		if err != nil {
+			log.Println(err.Error())
+			ctx.JSON(consts.PARSE_STRING_ERROR.Code, gin.H{
+				"message": consts.PARSE_STRING_ERROR.Message,
+				"detail":  err.Error(),
+			})
+			return
+		}
+		user := models.UserModel{
+			ChatID:     chatID,
+			Email:      email,
+			Username:   username,
+			FuckedUser: fuckedUser,
+		}
+
+		messages, err = wh.buyService.ProceedAfterPayment(user)
+		if err != nil {
+			log.Println(err.Error())
+			msg1 := tgbotapi.NewMessage(user.ChatID, err.Error())
+			msg2 := tgbotapi.NewMessage(user.ChatID, consts.INTERNAL_ERROR_CONTACT_SUPPORT_MESSGE)
+			if _, err := wh.Bot.Send(msg1); err != nil {
+				log.Println(err.Error())
+			}
+			if _, err := wh.Bot.Send(msg2); err != nil {
+				log.Println(err.Error())
+			}
+			return
+		}
+	default:
+		log.Println("Unsupported webhook type" + update.UpdateType)
 	}
 
 	for _, msg := range messages {
